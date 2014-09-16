ae5cbd5062f1940a52fba23b103a4c45f5907de4
Florent Gosselin <florent.gosselin@gmail.com>
2014-09-16 23:44:27 +0200

    Add box-sprint styles & chart for sprint status follow up

--- public/js/charts.js
+++ public/js/charts.js
@@ -24,3 +24,27 @@ var chart = c3.generate({
 });
 
 //Sprint status chart
+var chart = c3.generate({
+    bindto: '#sprint-charts',
+    data: {
+        columns: [
+            ['sprint', 35, 10, 7, 1, 2, 10]
+        ],
+        types: {
+            sprint: 'area-spline'
+        }
+    },
+    color: {
+        pattern: ['#6BAC1B', '#40BEFA', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+    },
+    point: {
+        r: 0
+    },
+    axis: {
+        x: { show: false },
+        y: { show: false }
+    },
+    legend: {
+        show: false
+    }
+});
